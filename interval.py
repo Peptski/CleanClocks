bcbce0a16b5aac499010423cb8776ea8d46b3b42
Lukas <lukas.s.andersson@gmail.com>
2022-02-10 12:12:02 +0100

    Update interval

--- interval.py
+++ interval.py
@@ -9,6 +9,8 @@ class Interval(QGroupBox):
     count = 0
     position = 0
     waitingInput = False
+    interval = 0
+    toast = ToastNotifier()
 
     def __init__(self, parent):
         super(Interval, self).__init__(parent)
@@ -24,7 +26,6 @@ class Interval(QGroupBox):
         lbl = QLabel()
         self.lbl = lbl
         lbl.setStyleSheet("color: white")
-        lbl.setFont(QFont('Serif', 43, QFont.Weight.Bold))
         layout.addWidget(lbl)
 
         timer = QTimer(self)
@@ -41,18 +42,19 @@ class Interval(QGroupBox):
                 self.position += 1
                 if self.position > 3:
                     self.count = (int(self.lbl.text()[0]) * 60000) + (int(self.lbl.text()[1]) * 6000) + (int(self.lbl.text()[3]) * 1000) + (int(self.lbl.text()[4]) * 100)
-                    self.waitingInput = False
+                    self.interval = self.count
                     self.flag = False
+                    self.waitingInput = False
                     self.position = 0
         else:
             if QMouseEvent.button() == Qt.MouseButton.LeftButton:
                 if self.count == 0:
                     self.waitingInput = True
+                    self.lbl.setFont(QFont('Serif', 44, QFont.Weight.Bold))
                     self.lbl.setText("00:00:xx")
                 self.flag = not self.flag
             elif QMouseEvent.button() == Qt.MouseButton.RightButton:
-                self.flag = False
-                self.count = 0
+                self.reset()
             elif QMouseEvent.button() == Qt.MouseButton.MiddleButton:
                 self.parent.delModule(self)
 
@@ -60,13 +62,14 @@ class Interval(QGroupBox):
         if self.flag: self.count -= 1
         if not self.waitingInput:
             if self.count == 1:
-                toast = ToastNotifier()
-                toast.show_toast("Timer finished!","Click the module to set a new timer",duration=10, threaded=True)
-            if self.count <= 0:
-                self.flag = False
-                self.count = 0
-                self.lbl.setText("Set time")
+                self.toast.show_toast("Interval lap!","Your set interval just made a lap!",duration=10, threaded=True)
+            elif self.count == 0 and self.flag == False:
+                self.lbl.setFont(QFont('Serif', 32, QFont.Weight.Bold))
+                self.lbl.setText("Set interval")
+            elif self.count < 0:
+                self.count = self.interval
             else:
+                self.lbl.setFont(QFont('Serif', 44, QFont.Weight.Bold))
                 t = ["00", ":", "00", ":", "00"]
                 if self.count % 100 < 10: t[4] = "0" + str(self.count % 100)
                 else: t[4] = str(self.count % 100)
@@ -102,3 +105,7 @@ class Interval(QGroupBox):
                 elif num > 9: num = "9"
                 else: num = str(num)
                 self.lbl.setText(self.lbl.text()[:4] + num + self.lbl.text()[5:])
+
+    def reset(self):
+        self.flag = False
+        self.count = 0
